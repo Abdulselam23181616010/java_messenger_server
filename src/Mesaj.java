ca5ebaa2e9b40c72db1fb352f41ef18353522756
Rist <rist05yt@gmail.com>
2024-12-22 22:46:54 +0300

    Mesaj sınıfına yorum satırlar eklendi

--- src/Mesaj.java
+++ src/Mesaj.java
@@ -1,13 +1,17 @@
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+//Mesajların id, gönderici time gibi özellikleri saklamak ve böylelikle iletişimin daha kolay olmasını amaçlayan bir sınıftır
 public class Mesaj implements Serializable {
+    //Karşı taraftaki Sınıfla uyuşmamazlığını gidermek amaçlı sınıfın ID'sini oluşturalım
     private static final long serialVersionUID = 7818547970534850237L;
     private int id;
     private String gonderici;
     private String mesaj;
     private LocalDateTime time;
 
+
+    //Constructorları oluşturalım
     public Mesaj(int id, String gonderici, String mesaj, LocalDateTime time) {
         this.id = id;
         this.gonderici = gonderici;
@@ -21,6 +25,7 @@ public class Mesaj implements Serializable {
 
     }
 
+    //Gerekli olan getterleri de beirtelim
     public String getGonderici() {
         return gonderici;
     }
